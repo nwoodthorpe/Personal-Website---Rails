d6161d0bff9ff086c32b3a0b96c5348dc378f0dc
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2016-10-15 10:14:23 -0400

    First test to test CI env

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PageControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index should render index success" do
+    get :index
+    assert_response :success
+  end
 end
