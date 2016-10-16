37026bf43e75eb755a4cfac5a6437483410238e7
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2016-10-16 12:11:30 -0400

    Add tests for links and projects

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :project
 
-  validates :project, :presence => true
+  validates :project, presence: true
+  validates :url, presence: true
+  validates :text, presence: true
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can create project" do
+    p = Project.new(title: "title", content: "content", position: 1)
+    assert p.save
+  end
+
+  test "Project must have title" do
+    p = Project.new(content: "Nope", position: 2)
+    refute p.save
+    assert p.errors[:title]
+  end
+
+  test "Project must have content" do
+    p = Project.new(title: "Nope", position: 2)
+    refute p.save
+    assert p.errors[:content]
+  end
+
+  test "Project must have position" do
+    p = Project.new(content: "Nope", title: "Title")
+    refute p.save
+    assert p.errors[:position]
+  end
+
+  test "Project must have unique position" do
+    p = Project.new(title: "title", content: "content", position: 1)
+    assert p.save
+
+    p2 = Project.new(title: "title2", content: "content2", position: 1)
+    refute p2.save
+    assert p2.errors[:position]
+  end
+
+  test "Project can have Link" do
+    p = Project.new(title: "title", content: "content", position: 1)
+    assert p.save
+    assert p.links.create(url: "url", text: "text")
+  end
 end
